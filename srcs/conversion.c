62eb0bf3dfdf295cdced0b046921badcac8fb0c9
BoatWRT <weerapatboat@gmail.com>
2023-04-28 14:16:37 +0700

    prototype

---
+++ srcs/conversion.c
@@ -0,0 +1,13 @@
+#include "ft_printf.h"
+#include "libft.h"
+
+size_t conversion_p(unsigned long long c, int f)
+
+
+size_t conversion_d(int n)
+
+
+size_t conversion_u(unsigned int n)
+
+
+size_t conversion_x(unsigned long long int, int f)
---
+++ srcs/ft_printf.c
@@ -0,0 +1,53 @@
+#include "ft_printf.h"
+#include "libft.h"
+#include <stdarg.h>
+
+int	ft_printf(const char *str, ...)
+{
+	va_list		args;
+	size_t			len;
+
+	len = 0;
+	if (!str)
+		return (NULL);
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			len = len + print_type(*(str + 1), &arg);
+			str++;
+		}
+		else
+			len = len + ft_putchar_fd(*str, 1)
+			str++;
+	}
+	va_end(args);
+	return (len);
+}
+
+size_t	print_type(int c, va_list *args)
+{
+	if (c == 'c')
+		len += ft_putchar_fd(va_arg(*args, int), 1);
+	else if(c == 's')
+		len += ft_putstr_fd(va_arg(*args, char *), 1);
+
+
+	else if(c == 'p')
+		len += conversion_p(va_arg(*args, int), 'p');
+
+	else if(c == 'i' || c == 'd')
+		len += conversion_d(va_arg(*args, int));
+
+
+	else if(c == 'u')
+		len += conversion_u(va_arg(*args, unsigned int));
+	else if(c == 'x')
+		len += conversion_x(va_arg(*args, unsigned long), 'x');
+	else if(c == 'X')
+		len += conversion_x(va_arg(*args, unsigned long), 'X');
+	else
+	len += ft_putchar_fd('%', 1);
+	return (len);
+}
