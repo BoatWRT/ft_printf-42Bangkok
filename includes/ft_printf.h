0fce8c242b7a93b0c9b919109bad509beb89a823
BoatWRT <weerapatboat@gmail.com>
2023-05-25 17:35:18 +0700

    add function to .h file

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,6 +6,11 @@
 # include <stddef.h>
 # include <stdlib.h>
 
-
+int		ft_printf(const char *str, ...)
+size_t	print_type(int c, va_list *args)
+size_t	conversion_p(unsigned long long c, int f)
+size_t	conversion_d(int n)
+size_t	conversion_u(unsigned int n)
+size_t	conversion_x(unsigned long long int, int f)
 
 #endif
--- libft/libft.h
+++ libft/libft.h
@@ -16,5 +16,6 @@ char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd)
 void	ft_putstr_fd(char *s, int fd)
 size_t	ft_strlen(const char *s);
+size_t	ft_baselen(size_t n, int base)
 
 #endif
